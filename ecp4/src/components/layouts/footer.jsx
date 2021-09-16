d58d71f7082a1192350d552e2465b6ac305b53ff
William Crews <crewswh@gmail.com>
2021-09-16 11:20:46 -0400

    works

---
+++ ecp4/src/components/layouts/footer.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+// import 'materialize-css';
+
+// const styles = {
+// footer: {
+//     position: 'absolute',
+//     left:0,
+//     bottom:0,
+//     right:0,
+
+// }
+
+// }
+
+const Footer = () => {
+    return (
+        <div>
+
+        </div>
+    )
+}
+
+export default Footer
---
+++ ecp4/src/components/layouts/navbar.jsx
@@ -0,0 +1,17 @@
+// Here Baby 2.0.1!!!
+
+import React from 'react';
+import { Component } from 'react';
+
+
+export class Navbar extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+};
+
+export default Navbar;
